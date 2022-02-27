84a2ff14f6a81b7254cebd851947ad9dd65f53e4
jburlette <jburlette@gmail.com>
2022-02-27 13:14:16 -0500

    initial commit: strange test behavior

---
+++ UtilityCloudChallenge/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UtilityCloudChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { -1, 2, 1, 4 };
+            int target = 1;
+
+            var sumFinder = new UtilityCloudChallenge.SumFinder();
+            Console.WriteLine($"Result: {sumFinder.SumThreeClosest(numbers, target)}");
+        }
+
+
+    }
+}
+
---
+++ UtilityCloudChallenge/SumFinder.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UtilityCloudChallenge
+{
+    public class SumFinder
+    {
+        private static List<int> Sums = new List<int>();
+
+        public int SumThreeClosest(int[] numbers, int target, bool recursive = true)
+        {
+            if (recursive)
+            {
+                //Sums = new List<int>();
+                CalculateSumsRecursive(numbers);
+            }
+            else
+            {
+                Sums = CalculateSums(numbers);
+            }
+            return Sums.OrderBy(sum => Math.Abs(target - sum)).First();
+        }
+
+        /*Nested Loops*/
+        private static List<int> CalculateSums(int[] numbers)
+        {
+            List<int> sums = new List<int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (j != i)
+                    {
+                        for (int k = 0; k < numbers.Length; k++)
+                        {
+                            if (k != i && k != j)
+                            {
+                                sums.Add(numbers[i] + numbers[j] + numbers[k]);
+                            }
+                        }
+                    }
+                }
+
+            }
+            return sums;
+        }
+
+        /* Recursive */
+        private static void CalculateSumsRecursive(int[] numbers, List<int> indexList = null)
+        {
+            indexList ??= new List<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (indexList.Contains(i))
+                {
+                    continue;
+                }
+
+                if (indexList.Count == 2)
+                {
+                    Sums.Add(numbers[i] + numbers[indexList[0]] + numbers[indexList[1]]);
+                }
+                else
+                {
+                    indexList.Add(i);
+                    CalculateSumsRecursive(numbers, indexList);
+                }
+            }
+        }
+    }
+}
